8e2e51ba65c8107611d8d72a7d172b3ec24882fc
pav-repo <pavpartbox@gmail.com>
2019-03-12 13:46:30 +0300

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+var money = +prompt('Ваш бюджет на месяц?','');
+var time = prompt('Введите дату в формате YYYY-MM-DD','');
+
+
+var appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income : [],
+	savings: false,
+};
+
+
+var a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+
+console.log(appData.budget / 30);
